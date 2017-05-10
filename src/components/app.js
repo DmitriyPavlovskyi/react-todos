4f26d1d8a67a90d64d874a5eaca810abd1e4725e
Dmitriy Pavlovskyi <dmitriyp@123software.ru>
2017-05-10 17:53:38 +0300

    list and list-item added

--- src/components/app.js
+++ src/components/app.js
@@ -12,12 +12,21 @@ const todos = [
   }
 ];
 
+// Конструктор нужен для того, что бы передать todos в наш компонент для рендера
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos
+    }
+  }
+
   render () {
     return (
       <div>
         <h1>React ToDos App</h1>
-        <TodosList />
+        <TodosList todos={this.state.todos}/>
       </div>
     );
   }
--- src/components/todos-list.js
+++ src/components/todos-list.js
@@ -1,11 +1,27 @@
+import _ from 'lodash';
 import React from 'react';
 import TodosListHeader from './todos-list-header';
+import TodosListItem from './todos-list-item';
 
+// Когда мы при создании компонента передаем в него какие-либо атрибуты, то они появятся в this.props
+// Тут мы будем выводить наши задачи из коллекции
+// ... -> ES6 spreading operator
+// task={todo.task} isCompleted={todo.isCompleted} === ...todo
 export default class TodosList extends React.Component {
+  renderItems() {
+    return _.map(this.props.todos, (todo, index) => <TodosListItem key={index} {...todo} />)
+  }
+
+
   render () {
+    console.log(this);
+    console.log(this.props.todos);
     return (
       <table>
         <TodosListHeader />
+        <tbody>
+          {this.renderItems()}
+        </tbody>
       </table>
     );
   }
